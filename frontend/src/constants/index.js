ffac5016e54cecdadfb13290e7e8428a15ee8497
Vedant Tilekar <vedanttilekar9417@gmail.com>
2025-07-26 12:20:11 +0530

    initial commit

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,103 @@
+export const THEMES = [
+    {
+        name: "light",
+        label: "Light",
+        colors: ["#ffffff", "#5a67d8", "#8b5cf6", "#1a202c"],
+    },
+    {
+        name: "dark",
+        label: "Dark",
+        colors: ["#1f2937", "#8b5cf6", "#ec4899", "#1a202c"],
+    },
+    
+    {
+        name: "forest",
+        label: "Forest",
+        colors: ["#1f1d1d", "#3ebc96", "#70c217", "#e2e8f0"],
+    },
+   
+    {
+        name: "corporate",
+        label: "Corporate",
+        colors: ["#ffffff", "#4b6bfb", "#7b92b2", "#1d232a"],
+    },
+    
+    {
+        name: "valentine",
+        label: "Valentine",
+        colors: ["#f0d6e8", "#e96d7b", "#a991f7", "#37243c"],
+    },
+    {
+        name: "halloween",
+        label: "Halloween",
+        colors: ["#0d0d0d", "#ff7800", "#006400", "#ffffff"],
+    },
+    
+    {
+        name: "aqua",
+        label: "Aqua",
+        colors: ["#193549", "#4cd4e3", "#9059ff", "#f8d766"],
+    },
+    
+    {
+        name: "fantasy",
+        label: "Fantasy",
+        colors: ["#ffe7d6", "#a21caf", "#3b82f6", "#f59e0b"],
+    },
+   
+    {
+        name: "black",
+        label: "Black",
+        colors: ["#000000", "#191919", "#313131", "#4a4a4a"],
+    },
+    {
+        name: "luxury",
+        label: "Luxury",
+        colors: ["#171618", "#1e293b", "#94589c", "#d4a85a"],
+    },
+    
+    {
+        name: "night",
+        label: "Night",
+        colors: ["#0f172a", "#38bdf8", "#818cf8", "#e2e8f0"],
+    },
+    {
+        name: "coffee",
+        label: "Coffee",
+        colors: ["#20161f", "#dd9866", "#497174", "#eeeeee"],
+    },
+];
+
+export const LANGUAGES = [
+    "English",
+    "Spanish",
+    "French",
+    "German",
+    "Mandarin",
+    "Japanese",
+    "Korean",
+    "Hindi",
+    "Russian",
+    "Portuguese",
+    "Arabic",
+    "Italian",
+    "Turkish",
+    "Dutch",
+];
+
+export const LANGUAGE_TO_FLAG = {
+    english: "gb",
+    spanish: "es",
+    french: "fr",
+    german: "de",
+    mandarin: "cn",
+    japanese: "jp",
+    korean: "kr",
+    hindi: "in",
+    russian: "ru",
+    portuguese: "pt",
+    arabic: "sa",
+    italian: "it",
+    turkish: "tr",
+    dutch: "nl",
+};
